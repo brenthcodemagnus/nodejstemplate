9551939d3121a23f461870da432550c734e95912
Jairo Malanay <jairo.malanay@beenest-tech.com>
2016-02-17 17:15:38 +0800

    es6 + trim user model

--- models/user.js
+++ models/user.js
@@ -1,29 +1,19 @@
 'use strict';
 
-module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User",
-        {
-            username: {
-                type: DataTypes.STRING   
-            }
-        },
-        {
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("User", {
+            username: { type: DataTypes.STRING }
+        }, {
             classMethods: {
-                associate: function(models) {
+                associate: (models) => {
                     User.hasMany(models.Task, {
-                            
                         onDelete: "CASCADE",
-                        
                         onUpdate: "CASCADE",
-
                         foreignKey: 'user_id',
-
                         targetKey: 'id'
-                    })
+                    });
                 }
             }
         }
     );
-
-    return User;
 };
